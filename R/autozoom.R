eba6f5ff63c76084e78f78996660f98c12bc3e91
johnhobbs <johnxhobbs@gmail.com>
2022-03-08 11:33:48 +0000

    autozoom() created as an idea

---
+++ R/autozoom.R
@@ -0,0 +1,10 @@
+#
+
+#' Auto Zoom to Create a Dynamic Plot
+#'
+#' Takes a plot function as its argument and repeats the call every time the plot
+#' is interacted with (click to zoom), using the new plot limits.
+#'
+autozoom = function(plt){
+  # Is it possible to pass plot() as an object? Worst case: pass as string and eval()
+}
